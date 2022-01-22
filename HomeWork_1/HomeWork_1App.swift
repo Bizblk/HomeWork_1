33557c051f3b0f8869093cbb545e412e002f5621
Bizblk <enemy_counter@mail.ru>
2022-01-22 11:55:07 +0300

    Initial Commit

---
+++ HomeWork_1/HomeWork_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork_1App.swift
+//  HomeWork_1
+//
+//  Created by Виталий Оранский on 22.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
